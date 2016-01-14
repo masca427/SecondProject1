478e3410ff5a3dd93a36710c19f6b12ce3eaeaf8
masca427 <masca427@gmail.com>
2016-01-14 15:33:50 +0900

    What?

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 	 System.out.println("Hello Git!");
 	 System.out.println("What is git?");
-	 System.out.println("Distributed Version Control System")
+	 System.out.println("Distributed Version Control System");
 	 }
 	}
